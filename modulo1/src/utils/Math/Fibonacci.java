a6ff9048baf6b7c0b8f80d1170b8598802da834f
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-11-04 00:12:07 -0600

    Actividad 12

---
+++ modulo1/src/utils/Math/Fibonacci.java
@@ -0,0 +1,39 @@
+package utils.Math;
+
+/**
+ * Clase que contiene métodos para generar la serie Fibonacci de manera iterativa y recursiva.
+ */
+public class Fibonacci {
+
+    /**
+     * Método para calcular el número de Fibonacci de forma iterativa.
+     *
+     * @param n El índice del número de Fibonacci.
+     * @return El número de Fibonacci correspondiente al índice dado.
+     */
+    public static int iterativeFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 0, b = 1, result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
+
+    /**
+     * Método para calcular el número de Fibonacci de forma recursiva.
+     *
+     * @param n El índice del número de Fibonacci.
+     * @return El número de Fibonacci correspondiente al índice dado.
+     */
+    public static int recursiveFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+    }
+}
