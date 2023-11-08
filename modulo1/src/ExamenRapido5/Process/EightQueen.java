9fcb640468fc519cb17ba1d376b573dd31f9d19d
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-11-08 14:47:27 -0600

    Examen rapido 5

--- modulo1/src/ExamenRapido5/Process/EightQueen.java
+++ modulo1/src/ExamenRapido5/Process/EightQueen.java
@@ -11,24 +11,29 @@ public class EightQueen{
         result = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         for(int i=0;i<n;i++)temp.add(0);
-        solveNQUtil(result,n,0,temp);
+        getAllSolutions(result,n,0,temp);
 
         return result;
     }
 
-    private static void solveNQUtil(List<List<Integer>> result,int n,int row,List<Integer> comb){
+
+    private static void getAllSolutions(List<List<Integer>> result,int n,int row,List<Integer> comb){
         if(row==n){
             result.add(new ArrayList<>(comb));
             return;
         }
         for(int col = 0;col<n;col++){
-            if(cols[col] || leftDiagonal[row+col] || rightDiagonal[row-col+n])
+            if(isValidPlace(col, row, n))
                 continue;
 
             cols[col] = leftDiagonal[row+col] = rightDiagonal[row-col+n] = true;
             comb.set(col,row+1);
-            solveNQUtil(result,n,row+1,comb);
+            getAllSolutions(result,n,row+1,comb);
             cols[col] = leftDiagonal[row+col] = rightDiagonal[row-col+n] = false;
         }
+    }
+
+    private static boolean isValidPlace(int col, int row, int n){
+        return cols[col] || leftDiagonal[row+col] || rightDiagonal[row-col+n];
     }
 }
